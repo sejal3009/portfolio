28792fc5b6c26e2b58a66226fbf59efce9603417
sejal <sejal3009@gmail.com>
2021-05-21 23:37:28 +0530

    first commit

--- website/urls.py
+++ website/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from myapp import views
+from website.myapp import urls as app_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('index/', views.index2, name='Home'),
